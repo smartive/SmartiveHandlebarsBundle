ee7ba9cc04ff8a17705f632bf8fa1664e01ad201
Moreno Feltscher <moreno@smartive.ch>
2015-04-09 09:33:03 +0200

    Adding compiler pass for Handlebars helpers

--- SmartiveHandlebarsBundle.php
+++ SmartiveHandlebarsBundle.php
@@ -2,8 +2,19 @@
 
 namespace Smartive\HandlebarsBundle;
 
+use Smartive\HandlebarsBundle\DependencyInjection\HelperCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SmartiveHandlebarsBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new HelperCompilerPass());
+    }
 }
